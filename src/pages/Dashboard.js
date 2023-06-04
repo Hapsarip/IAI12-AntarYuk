9829123e38e31923ff19ab9d1b9411946c183b30
Hapsarip <hapsari.ndhari7@gmail.com>
2023-06-04 12:14:11 +0700

    fixing make order page

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,14 @@
 import React from "react";
 import Motor from "../img/motor.png"
+import { useNavigate } from "react-router-dom";
 
 export default function Dashboard () {
+    const navigate = useNavigate();
+    const navigateToOrder = () => {
+        navigate('/MakeOrder');
+    };
+
+    
     return (
         <div className="bg-gradient-to-br from-white via-slate-200 to-blue-800">
             <div className="flex items-center w-full h-screen px-[100px]">
@@ -13,11 +20,12 @@ export default function Dashboard () {
                     <div className="w-full text-black text-2xl text-left my-10 font-muli">
                         Pesan makanan dengan mudah dengan layanan pengantaran makanan kami yang cepat, praktis, dan terpercaya.
                     </div>
-                    <button className="place-items-start bg-red-600 w-[200px] px-3 py-2 rounded-full font-muli text-lg text-white hover:bg-red-400">
+                    <button className="place-items-start bg-red-600 w-[200px] px-3 py-2 rounded-full font-muli text-lg text-white hover:bg-red-400"
+                        onClick={navigateToOrder}>
                         Order Sekarang
                     </button>
                 </div>
-                <div className="hidden lg:flex h-full w-2/5 items-center justify-center">
+                <div className="hidden lg:flex h-full w-2/5 items-center justify-center" >
                     <img class="justify-center items-center mx-10 mt-10" src={Motor} alt="bgImg"/>
                 </div>
             </div>
--- src/pages/Make-Order.js
+++ src/pages/Make-Order.js
@@ -1,73 +1,98 @@
-import React from "react";
-import { Link } from 'react-router-dom';
+import {React, useState} from "react";
+import { useNavigate } from "react-router-dom";
 import Food1 from '../img/food1.png'
+import Food2 from '../img/food2.png'
+import Food3 from '../img/food3.png'
+import Food4 from '../img/food4.png'
+import Food5 from '../img/food5.png'
 
 export default function MakeOrder () {
+    const navTrack = useNavigate();
+    const navigateToTrack = () => {
+        navTrack('/Track');
+    };
+
     return (
         <div className="flex items-start w-full h-screen bg-slate-100">
             <div className="w-1/2 mx-[20px]">
                 <div className='grid items-center overflow-hidden flex justify-between bg-slate-100 w-full p-4 border-2 border-gray-100 mx-auto'>
                     <h1 className='justify-center py-4 px-5 items-center text-left text-2xl font-medium'>Pilih Restoran</h1>
                     <ul>
-                        <button className="flex place-items-center w-[600px] bg-white px-7 py-5 mb-3 rounded-xl font-muli text-lg text-black focus:bg-slate-300">
-                            <img className="justify-center items-center h-auto max-w-full mr-7" src={Food1} alt="Img"/>
+                        <li>
+                        <input type="radio" id="hosting-a" name="hosting" value="hosting-a" class="hidden peer" required/>
+                        <label for="hosting-a" className="flex place-items-center w-[600px] bg-white px-7 py-5 mb-3 rounded-xl font-muli text-lg text-black peer-checked:bg-slate-300">
+                            <img className="justify-center items-center h-auto max-w-full mr-10" src={Food1} alt="Img"/>
                             <div className=" grid justify-items-start">
                                 <h3>
                                     Restoran A
                                 </h3>
                                 <h3>
-                                    1.2 km
+                                    2.5 km
                                 </h3>
                             </div>
-                        </button>
-                        <button className="flex place-items-center w-[600px] bg-white px-7 py-5 mb-3 rounded-xl font-muli text-lg text-black focus:bg-slate-300">
-                            <img className="justify-center items-center h-auto max-w-full mr-7" src={Food1} alt="Img"/>
+                        </label>
+                        </li>
+                        <li>
+                        <input type="radio" id="hosting-b" name="hosting" value="hosting-b" class="hidden peer"/>
+                        <label for="hosting-b" className="flex place-items-center w-[600px] bg-white px-7 py-5 mb-3 rounded-xl font-muli text-lg text-black peer-checked:bg-slate-300">
+                            <img className="justify-center items-center h-auto max-w-full mr-7" src={Food2} alt="Img"/>
                             <div className=" grid justify-items-start">
                                 <h3>
                                     Restoran B
                                 </h3>
                                 <h3>
-                                    1.2 km
+                                    2.1 km
                                 </h3>
                             </div>
-                        </button>
-                        <button className="flex place-items-center w-[600px] bg-white px-7 py-5 mb-3 rounded-xl font-muli text-lg text-black focus:bg-slate-300">
-                            <img className="justify-center items-center h-auto max-w-full mr-7" src={Food1} alt="Img"/>
+                        </label>
+                        </li>
+                        <li>
+                        <input type="radio" id="hosting-c" name="hosting" value="hosting-c" class="hidden peer" required/>
+                        <label for="hosting-c" className="flex place-items-center w-[600px] bg-white px-7 py-5 mb-3 rounded-xl font-muli text-lg text-black peer-checked:bg-slate-300">
+                            <img className="justify-center items-center h-auto max-w-full mr-7" src={Food3} alt="Img"/>
                             <div className=" grid justify-items-start">
                                 <h3>
                                     Restoran C
                                 </h3>
                                 <h3>
-                                    1.2 km
+                                    1.5 km
                                 </h3>
                             </div>
-                        </button>
-                        <button className="flex place-items-center w-[600px] bg-white px-7 py-5 mb-3 rounded-xl font-muli text-lg text-black focus:bg-slate-300">
-                            <img className="justify-center items-center h-auto max-w-full mr-7" src={Food1} alt="Img"/>
+                        </label>
+                        </li>
+                        <li>
+                        <input type="radio" id="hosting-d" name="hosting" value="hosting-d" class="hidden peer" required/>
+                        <label for="hosting-d" className="flex place-items-center w-[600px] bg-white px-7 py-5 mb-3 rounded-xl font-muli text-lg text-black peer-checked:bg-slate-300">
+                            <img className="justify-center items-center h-auto max-w-full mr-7" src={Food4} alt="Img"/>
                             <div className=" grid justify-items-start">
                                 <h3>
                                     Restoran D
                                 </h3>
                                 <h3>
-                                    1.2 km
+                                    3.7 km
                                 </h3>
                             </div>
-                        </button>
-                        <button className="flex place-items-center w-[600px] bg-white px-7 py-5 mb-3 rounded-xl font-muli text-lg text-black focus:bg-slate-300">
-                            <img className="justify-center items-center h-auto max-w-full mr-7" src={Food1} alt="Img"/>
+                        </label>
+                        </li>
+                        <li>
+                        <input type="radio" id="hosting-e" name="hosting" value="hosting-e" class="hidden peer" required/>
+                        <label for="hosting-e" className="flex place-items-center w-[600px] bg-white px-7 py-5 mb-3 rounded-xl font-muli text-lg text-black peer-checked:bg-slate-300">
+                            <img className="justify-center items-center h-auto max-w-full mr-7" src={Food5} alt="Img"/>
                             <div className=" grid justify-items-start">
                                 <h3>
                                     Restoran E
                                 </h3>
                                 <h3>
-                                    1.2 km
+                                    2.3 km
                                 </h3>
                             </div>
-                        </button>
+                        </label>
+                        </li>
                     </ul>
                 </div>
             </div>
             <div className="grid w-1/2 mx-[20px]">
+            
                 <div className='grid items-start overflow-hidden flex justify-between bg-slate-100 w-full p-4 border-2 border-gray-100 mx-auto'>
                     <h1 className='justify-center py-4 px-5 items-center text-left text-2xl font-medium'>Tambah Catatan</h1>
                     <form>
@@ -78,19 +103,37 @@ export default function MakeOrder () {
                 </div>
                 <div className='grid items-start overflow-hidden flex justify-between bg-slate-100 w-full p-4 border-2 border-gray-100 mx-auto'>
                     <h1 className='justify-center py-4 px-5 items-center text-left text-2xl font-medium'>Pilih Metode Pembayaran</h1>
-                    <button className=" ml-5 flex place-items-center w-[550px] bg-white px-7 py-5 rounded-xl font-muli text-lg text-slate-400 hover:bg-slate-300">
-                        Pilih metode pembayaran di sini
-                    </button>
+                    <ul class="ml-5 grid w-full gap-3 md:grid-cols-2">
+                        <div class="flex items-center pl-4 bg-white rounded-xl w-[260px]">
+                            <input id="radio-pay-a" type="radio" value="" name="bordered-radio" class="w-4 h-4 text-blue-600 bg-white border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                            <label for="radio-pay-a" class="ml-5 flex place-items-center w-full bg-white px-7 py-5 rounded-xl font-muli text-lg text-black">Tunai</label>
+                        </div>
+                        <div class="flex items-center pl-4 bg-white rounded-xl w-[260px]">
+                            <input id="radio-pay-b" type="radio" value="" name="bordered-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                            <label for="radio-pay-b" class="ml-5 flex place-items-center w-full bg-white px-7 py-5 rounded-xl font-muli text-lg text-black">Dompet virtual</label>
+                        </div>
+                    </ul>
                 </div>
                 <div className='grid items-start overflow-hidden flex justify-between bg-slate-100 w-full p-4 border-2 border-gray-100 mx-auto'>
                     <h1 className='justify-center py-4 px-5 items-center text-left text-2xl font-medium'>Pilih Jenis Pengiriman</h1>
-                    <button className=" ml-5 flex place-items-center w-[550px] bg-white px-7 py-5 rounded-xl font-muli text-lg text-slate-400 hover:bg-slate-300">
-                        Pilih jenis pengiriman di sini
+                    <ul class="ml-5 grid w-full gap-3 md:grid-cols-2">
+                        <div class="flex items-center pl-4 bg-white rounded-xl w-[260px]">
+                            <input id="radio-deliver-a" type="radio" value="" name="radio-deliver" class="w-4 h-4 text-blue-600 bg-white border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                            <label for="radio-pay-a" class="ml-5 flex place-items-center w-full bg-white px-7 py-5 rounded-xl font-muli text-lg text-black">Kilat</label>
+                        </div>
+                        <div class="flex items-center pl-4 bg-white rounded-xl w-[260px]">
+                            <input id="radio-deliver-b" type="radio" value="" name="radio-deliver" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                            <label for="radio-deliver-b" class="ml-5 flex place-items-center w-full bg-white px-7 py-5 rounded-xl font-muli text-lg text-black">Santai</label>
+                        </div>
+                    </ul>
+                </div>
+                <div className="ml-[375px] mt-5">
+                    <button className="bg-red-600 w-[200px] px-3 py-2 rounded-full font-muli text-lg text-white hover:bg-red-400"
+                        onClick={navigateToTrack}>
+                        Order
                     </button>
                 </div>
-                <button className="place-items-end bg-red-600 w-[200px] px-3 py-2 rounded-full font-muli text-lg text-white hover:bg-red-400">
-                    Order
-                </button>
+                
             </div>
         </div>
     )
